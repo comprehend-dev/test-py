f989ab3a2d041db322eb5efc861af37276900753
Jonathan Worthington <jnthn@jnthn.net>
2024-02-22 15:11:04 +0100

    Add another function and a class

---
+++ db/repo.py
@@ -0,0 +1,3 @@
+class Repository:
+    def get_item(self, item_id: int):
+        return {"id": item_id, "name": "Foo"}
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from db.repo import Repository
 
 app = FastAPI()
-
 
 @app.get("/")
 async def check():
     return {"stats": "ok"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    repo = Repository()
+    return repo.get_item(item_id)
